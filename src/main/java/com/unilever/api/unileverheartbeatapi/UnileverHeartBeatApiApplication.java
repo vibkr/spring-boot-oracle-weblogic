5943609e9336ccd37ee7eac8bbcdf9356b9e25be
VIBKUMAR-IN\vibkumar <vibhash.kumar@oracle.com>
2012-03-08 16:23:28 +0530

    working war for Dev

--- src/main/java/com/unilever/api/unileverheartbeatapi/UnileverHeartBeatApiApplication.java
+++ src/main/java/com/unilever/api/unileverheartbeatapi/UnileverHeartBeatApiApplication.java
@@ -1,17 +1,29 @@
 package com.unilever.api.unileverheartbeatapi;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
-public class UnileverHeartbeatApiApplication {
+@Configuration
+public class UnileverHeartBeatApiApplication extends SpringBootServletInitializer implements WebApplicationInitializer {
 
+    // Tell Spring to launch our app!
     public static void main(String[] args) {
-        SpringApplication.run(UnileverHeartbeatApiApplication.class, args);
+        SpringApplication.run(UnileverHeartBeatApiApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(UnileverHeartBeatApiApplication.class);
     }
 
     @Bean
